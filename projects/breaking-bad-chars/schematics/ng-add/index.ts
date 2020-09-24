ea9fb546d2cb50146127b22989ce91aec860dcd6
mharb <mohamed.harb@cegedim.com>
2020-09-24 16:25:50 +0200

    Modified schematics for publishing

--- projects/breaking-bad-chars/schematics/ng-add/index.ts
+++ projects/breaking-bad-chars/schematics/ng-add/index.ts
@@ -1,10 +1,13 @@
-import { chain, noop, Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { chain, noop, Rule, SchematicContext, Tree, SchematicsException } from '@angular-devkit/schematics';
 import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
-
-import {
-  NodeDependencyType, NodeDependency, addPackageJsonDependency,
-  WorkspaceProject, getWorkspace, getProjectFromWorkspace, addModuleImportToRootModule
-} from 'schematics-utilities';
+import { NodeDependency, NodeDependencyType, addPackageJsonDependency } from '@schematics/angular/utility/dependencies';
+import { JsonParseMode, parseJson } from '@angular-devkit/core';
+import { InsertChange } from '@schematics/angular/utility/change';
+import { getAppModulePath } from '@schematics/angular/utility/ng-ast-utils';
+import { WorkspaceProject, WorkspaceSchema } from '@angular-devkit/core/src/experimental/workspace';
+import { SourceFile } from 'typescript';
+import { addImportToModule } from '@schematics/angular/utility/ast-utils';
+import { createSourceFile, ScriptTarget } from 'typescript';
 
 const PROJECTS_KEY = 'projects';
 
@@ -19,7 +22,7 @@ export function addChars(options: any): Rule {
 function addPackageJsonDependencies(): Rule {
   return (host: Tree, context: SchematicContext) => {
     const dependencies: NodeDependency[] = [
-      { type: NodeDependencyType.Default, version: '~0.0.1', name: 'breaking-bad-chars' }
+      { type: NodeDependencyType.Default, version: '^0.0.1', name: 'breaking-bad-chars' }
     ];
 
     dependencies.forEach(dependency => {
@@ -42,18 +45,105 @@ function installPackageJsonDependencies(): Rule {
 
 function addModuleToImports(options: any): Rule {
   return (host: Tree, context: SchematicContext) => {
-    const workspace = getWorkspace(host);
-    const project: any = getProjectFromWorkspace(
+    const workspace = SchematicsUtilities.getWorkspace(host);
+    const project: any = SchematicsUtilities.getProjectFromWorkspace(
       workspace,
       options.project ? options.project : Object.keys(workspace[PROJECTS_KEY])[0]
     );
     const moduleName = 'BreakingBadCharsModule';
 
-    const castedProject: WorkspaceProject = project as WorkspaceProject;
-
-    addModuleImportToRootModule(host, moduleName, 'breaking-bad-chars', castedProject);
+    SchematicsUtilities.addModuleImportToRootModule(host, moduleName, 'breaking-bad-chars', project);
     context.logger.log('info', `✅️ "${moduleName}" is imported`);
 
     return host;
   };
+}
+
+export class SchematicsUtilities {
+
+  static getWorkspacePath(host: Tree): string {
+    const possibleFiles = ['/angular.json', '/.angular.json'];
+    const path = possibleFiles.filter(p => host.exists(p))[0];
+    return path;
+  }
+
+  static getWorkspace(host: Tree): WorkspaceSchema {
+    const path = SchematicsUtilities.getWorkspacePath(host);
+    const configBuffer = host.read(path);
+    if (configBuffer === null) {
+      throw new SchematicsException(`Could not find (${path})`);
+    }
+    const content = configBuffer.toString();
+
+    return (parseJson(content, JsonParseMode.Loose) as {}) as WorkspaceSchema;
+  }
+
+  static getProjectFromWorkspace(workspace: WorkspaceSchema, projectName?: string): WorkspaceProject {
+    const name: string = (projectName) ? projectName : (workspace.defaultProject as string);
+    const project = workspace.projects[name];
+
+    if (!project) {
+      throw new SchematicsException(`Could not find project in workspace: ${projectName}`);
+    }
+
+    return project;
+  }
+
+
+  static addModuleImportToRootModule(host: Tree, moduleName: string, src: string, project: WorkspaceProject): void {
+    const modulePath = getAppModulePath(host, SchematicsUtilities.getProjectMainFile(project));
+    SchematicsUtilities.addModuleImportToModule(host, modulePath, moduleName, src);
+  }
+
+  static getProjectTargetOptions(project: WorkspaceProject, buildTarget: string): any {
+    if (project.targets && project.targets[buildTarget] && project.targets[buildTarget].options) {
+      return project.targets[buildTarget].options;
+    }
+
+    if (project.architect && project.architect[buildTarget] && project.architect[buildTarget].options) {
+      return project.architect[buildTarget].options;
+    }
+
+    throw new SchematicsException(`Cannot determine project target configuration for: ${buildTarget}.`);
+  }
+
+  static getProjectMainFile(project: WorkspaceProject): string {
+    const buildOptions = SchematicsUtilities.getProjectTargetOptions(project, 'build');
+
+    if (!buildOptions.main) {
+      throw new SchematicsException(
+        `Could not find the project main file inside of the ` + `workspace config (${project.sourceRoot})`
+      );
+    }
+
+    return buildOptions.main;
+  }
+
+  static getSourceFile(host: Tree, path: string): SourceFile {
+    const buffer = host.read(path);
+    if (!buffer) {
+      throw new SchematicsException(`Could not find file for path: ${path}`);
+    }
+    return createSourceFile(path, buffer.toString(), ScriptTarget.Latest, true);
+  }
+
+  static addModuleImportToModule(host: Tree, modulePath: string, moduleName: string, src: string): void {
+    const moduleSource: any = SchematicsUtilities.getSourceFile(host, modulePath);
+
+    if (!moduleSource) {
+      throw new SchematicsException(`Module not found: ${modulePath}`);
+    }
+
+    const changes = addImportToModule(moduleSource, modulePath, moduleName, src);
+    const recorder = host.beginUpdate(modulePath);
+
+    changes.forEach(change => {
+      if (change instanceof InsertChange) {
+        recorder.insertLeft(change.pos, change.toAdd);
+      }
+    });
+
+    host.commitUpdate(recorder);
+  }
+
 }
